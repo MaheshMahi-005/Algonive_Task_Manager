641365a053d983c936f9c014f5b2af0137d1a589
Mahesh Chandra <gmaheshchandra75@gmail.com>
2026-01-16 21:39:14 +0530

    added Js

---
+++ script.js
@@ -0,0 +1,84 @@
+// {
+//     id:12345;
+//     title:"Study JS",
+//     description:"Finish Arrays",
+//     dueDate:"2026-01-30",
+//     completed:false
+// }
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+function addTask() {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const dueDate = document.getElementById("dueDate").value;
+  if (!title || !dueDate) {
+    alert("Title and Due Date are required");
+    return;
+  }
+  const task = {
+    id: Date.now(),
+    title,
+    description,
+    dueDate,
+    completed: false,
+  };
+  tasks.push(task);
+  saveTasks();
+  displayTasks(tasks);
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function displayTasks(taskArray) {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  taskArray.forEach((task) => {
+    const div = document.createElement("div");
+    div.className = "task" + (task.completed ? " completed" : "");
+    div.innerHTML = `
+    <h3>${task.title}</h3>
+    <p>${task.description}</p>
+    <p>Due :${task.dueDate}</p>
+    <button onclick="toggleTask(${task.id})">Completed</button>
+    <button onclick="deleteTask(${task.id})">Delete</button>
+`;
+    taskList.appendChild(div);
+  });
+}
+
+function deleteTask(id) {
+  tasks = tasks.filter((task) => task.id !== id);
+  saveTasks();
+  displayTasks(tasks);
+}
+
+function toggleTask(id) {
+  tasks = tasks.map((task) =>
+    task.id === id ? { ...task, completed: !task.completed } : task
+  );
+  saveTasks();
+  displayTasks(tasks);
+}
+
+function filterTasks(type) {
+  if (type === "completed") {
+    displayTasks(tasks.filter((t) => t.completed));
+  } else if (type === "pending") {
+    displayTasks(tasks.filter((t) => !t.completed));
+  } else {
+    displayTasks(tasks);
+  }
+}
+
+function checkReminders() {
+  const today = new Date().toISOString().split("T")[0];
+  tasks.forEach((task) => {
+    if (task.dueDate === today && !task.completed) {
+      alert(`Reminder:"${task.title}"is due today!`);
+    }
+  });
+}
+
+displayTasks(tasks);
+checkReminders();
